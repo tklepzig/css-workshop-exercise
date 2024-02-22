9b673150d3248f9a9322f595472b96af4a54ff1a
Johannes Lüdke <johannes.luedke@invision.de>
2024-02-22 10:28:49 +0100

    add kings cross first example

--- src/public/components/teams/kings-cross/index.tsx
+++ src/public/components/teams/kings-cross/index.tsx
@@ -1,1 +1,16 @@
-export const Index = () => <div>Dummy</div>;
+import { cx } from "class-variance-authority"
+
+export const Index = () => (
+  <div className="p-3">
+    <ul className="h-[10rem] w-[33%] overflow-auto border">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <li
+          key={i}
+          className={cx("px-1 py-3 odd:bg-ui-100", i === 4 && "sticky top-0 ")}
+        >
+          Item {i}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
