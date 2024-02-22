f1eb8f46844ab2d1fe77babf342599654bd8760d
Heiko Rabe <heiko.rabe@invision.de>
2024-02-22 10:44:35 +0100

    Finalize task 1

--- src/public/components/teams/ravenclaw/index.tsx
+++ src/public/components/teams/ravenclaw/index.tsx
@@ -1,1 +1,12 @@
-export const Index = () => <div>Dummy</div>;
+import { cx } from "class-variance-authority";
+
+export const Index = () => (
+  <div className={'m-2'}>
+    <ul className={'h-[10rem] w-1/3 overflow-auto border'}>
+      {Array.from({ length: 10 }).map((_, i) => (
+        // eslint-disable-next-line react/jsx-key
+        <li className={cx('bg-ui-0 px-1 py-3 even:bg-ui-200', i === 4 && 'sticky top-0')}>Item {i}</li>
+      ))}
+    </ul>
+  </div>
+);
