35fcee0ef98380cd96a0cabea1eaf4bccea4bb39
Heiko Rabe <heiko.rabe@invision.de>
2024-02-22 17:08:11 +0100

    Simplify Excercise4 Task1

--- src/public/components/teams/ravenclaw/Exercise4.tsx
+++ src/public/components/teams/ravenclaw/Exercise4.tsx
@@ -3,17 +3,17 @@ import { Skeleton } from "../../Skeleton";
 const Task1 = () => (
   <section>
     <h2 className="mb-2">Task 1</h2>
-    <div className="grid grid-cols-[10rem_1fr_4rem] gap-3 p-3">
-      <img className="row-span-4" src="/assets/picture.jpg" alt="" />
-      <Skeleton className="h-[2.7rem]" />
-      <Skeleton className="h-[2.7rem]" />
-      <Skeleton className="col-span-2 h-[2.7rem]" />
-      <Skeleton className="col-span-2 h-[2.7rem]" />
-      <Skeleton className="col-span-2 h-[2.7rem]" />
-      <Skeleton className="col-span-2 h-[2.7rem]" />
-      <Skeleton className="h-[2.7rem]" />
-      <Skeleton className="h-[2.7rem]" />
-      <Skeleton className="col-span-2 h-[2.7rem]" />
+    <div className="grid auto-rows-fr grid-cols-[10rem_1fr_4rem] gap-3 p-3">
+      <img className="row-span-3" src="/assets/picture.jpg" alt="" />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton className="col-span-2" />
+      <Skeleton className="col-span-2" />
+      <Skeleton className="col-span-2 col-start-2" />
+      <Skeleton className="col-span-2" />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton className="col-span-2" />
     </div>
   </section>
 );
