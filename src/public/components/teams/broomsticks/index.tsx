6e31fb293bb17b236b956def1eb87a8f3c3f7161
Jens Zobel <jens.zobel@injixo.com>
2024-02-22 14:22:21 +0100

    Richtig schön committen.

--- src/public/components/teams/broomsticks/index.tsx
+++ src/public/components/teams/broomsticks/index.tsx
@@ -51,6 +51,51 @@ const E2T4 = () => (
   </ul>
 );
 
+const E3T1 = () => (
+  <div className="flex h-7 w-[33vw] flex-wrap items-end justify-end overflow-auto bg-ui-200 pl-7">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  </div>
+);
+
+const E3T2 = () => (
+  <div className="flex h-[80vh] flex-col justify-between gap-2 overflow-auto sm:flex-row sm:flex-wrap sm:items-center">
+    {Array.from({ length: 30 }).map((_, i) => (
+      // eslint-disable-next-line react/jsx-key
+      <Skeleton>Item {i}</Skeleton>
+    ))}
+  </div>
+);
+
+const E3T3 = () => (
+  <div
+    className={cx(
+      "flex h-[33vh] items-center justify-between",
+      "border",
+      "px-5"
+    )}
+  >
+    <Skeleton className="self-end">Item 0</Skeleton>
+    <Skeleton>Item 12</Skeleton>
+    <Skeleton className="self-start">Item E</Skeleton>
+    <Skeleton>Item A</Skeleton>
+    <Skeleton className="self-end">Item S</Skeleton>
+  </div>
+);
+
+const E3T4 = () => (
+  <div
+    className={cx(
+      "flex h-[80vh] flex-col gap-2 md:flex-row",
+      "overflow-auto md:items-start md:justify-end"
+    )}
+  >
+    {Array.from({ length: 10 }).map((_, i) => (
+      // eslint-disable-next-line react/jsx-key
+      <Skeleton>Item {i}</Skeleton>
+    ))}
+  </div>
+);
+
 export const Index = () => (
   <>
     <h1>Exercise 1</h1>
@@ -65,5 +110,15 @@ export const Index = () => (
     <E2T3 />
     <h2>Task 4</h2>
     <E2T4 />
+
+    <h1>Exercise 3</h1>
+    <h2>Task 1</h2>
+    <E3T1 />
+    <h2>Task 2</h2>
+    <E3T2 />
+    <h2>Task 3</h2>
+    <E3T3 />
+    <h2>Task 4</h2>
+    <E3T4 />
   </>
 );
