965c65d9fe74090ec2190d126b233e1f2c52ce35
Andreas Haller <andreas.haller@invision.de>
2024-02-22 10:35:17 +0100

    Implement task 1

--- src/public/components/teams/phoenix/index.tsx
+++ src/public/components/teams/phoenix/index.tsx
@@ -1,1 +1,10 @@
-export const Index = () => <div>Dummy</div>;
+export const Index = () => (
+  <div className="h-[10rem] w-1/3 overflow-auto">
+    <ul className="[&>*:nth-child(4)]:sticky [&>*:nth-child(4)]:top-0">
+      {Array.from({ length: 10 }).map((_, i) => (
+        // eslint-disable-next-line react/jsx-key
+        <li className="bg-ui-200 p-2 even:bg-ui-100">Item {i}</li>
+      ))}
+    </ul>
+  </div>
+);
