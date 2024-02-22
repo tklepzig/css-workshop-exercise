19d736d24846fd9f7c2b6d488d9fac95aeed322e
Heiko Rabe <heiko.rabe@invision.de>
2024-02-22 16:34:42 +0100

    Add Exercise4 - Task1

--- src/public/components/teams/ravenclaw/Exercise4.tsx
+++ src/public/components/teams/ravenclaw/Exercise4.tsx
@@ -1,6 +1,20 @@
+import { Skeleton } from "../../Skeleton";
+
 const Task1 = () => (
   <section>
     <h2 className="mb-2">Task 1</h2>
+    <div className="grid grid-cols-[10rem_1fr_4rem] gap-3 p-3">
+      <img className="row-span-4" src="/assets/picture.jpg" alt="" />
+      <Skeleton className="h-[2.7rem]" />
+      <Skeleton className="h-[2.7rem]" />
+      <Skeleton className="col-span-2 h-[2.7rem]" />
+      <Skeleton className="col-span-2 h-[2.7rem]" />
+      <Skeleton className="col-span-2 h-[2.7rem]" />
+      <Skeleton className="col-span-2 h-[2.7rem]" />
+      <Skeleton className="h-[2.7rem]" />
+      <Skeleton className="h-[2.7rem]" />
+      <Skeleton className="col-span-2 h-[2.7rem]" />
+    </div>
   </section>
 );
 
--- src/public/components/teams/ravenclaw/index.tsx
+++ src/public/components/teams/ravenclaw/index.tsx
@@ -1,11 +1,13 @@
 import { Exercise1 } from "./Exercise1";
 import { Exercise2 } from "./Exercise2";
 import { Exercise3 } from "./Exercise3";
+import { Exercise4 } from "./Exercise4";
 
 export const Index = () => (
   <div className="m-2">
     <Exercise1 />
     <Exercise2 />
     <Exercise3 />
+    <Exercise4 />
   </div>
 );
